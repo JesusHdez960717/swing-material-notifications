5a181249f999e3f18dcb5080bccfb1535b1e2492
Unknown <unknown@example.com>
2021-01-07 22:19:35 -0500

    Licence a clases

--- src/main/java/com/root101/swing/notification/toast/DialogToast.java
+++ src/main/java/com/root101/swing/notification/toast/DialogToast.java
@@ -1,12 +1,26 @@
+/*
+ * Copyright 2021 Root101 (jhernandezb96@gmail.com, +53-5-426-8660).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Or read it directly from LICENCE.txt file at the root of this project.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.root101.swing.notification.toast;
 
 import com.jhw.module.util.personalization.core.domain.Personalization;
 import com.jhw.module.util.personalization.services.PersonalizationHandler;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JDialog;
-import javax.swing.JPanel;
 import javax.swing.Timer;
 import com.root101.swing.material.standards.MaterialColors;
 import com.root101.swing.notification.NotificationLocation;
@@ -16,7 +30,6 @@ import com.root101.swing.util.SafePropertySetter;
 import com.root101.swing.util.Utils;
 import java.awt.BorderLayout;
 import java.awt.Toolkit;
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -29,7 +42,8 @@ import org.jdesktop.core.animation.timing.interpolators.SplineInterpolator;
  * Si NO tiene titulo se undecora.
  *
  *
- * @author Jesús Hernandez Barrios (jhernandezb96@gmail.com)
+ * @author Root101 (jhernandezb96@gmail.com, +53-5-426-8660)
+ * @author JesusHdezWaterloo@Github
  */
 public class DialogToast extends JDialog {
 
--- src/main/java/com/root101/swing/notification/toast/types/text/TextToast.java
+++ src/main/java/com/root101/swing/notification/toast/types/text/TextToast.java
@@ -1,10 +1,25 @@
+/*
+ * Copyright 2021 Root101 (jhernandezb96@gmail.com, +53-5-426-8660).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Or read it directly from LICENCE.txt file at the root of this project.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.root101.swing.notification.toast.types.text;
 
 import com.root101.swing.notification.toast.ToastComponent;
 import java.awt.*;
 import java.awt.geom.RoundRectangle2D;
 import com.root101.swing.material.effects.*;
-import com.root101.swing.material.standards.MaterialColors;
 import com.root101.swing.material.standards.MaterialFontRoboto;
 import com.root101.swing.material.standards.MaterialShadow;
 import com.root101.swing.util.MaterialDrawingUtils;
@@ -16,6 +31,9 @@ import com.root101.swing.util.Utils;
  * @see <a
  * href="https://www.google.com/design/spec/components/snackbars-toasts.html">Snackbars
  * and toasts</a>
+ * 
+ * @author Root101 (jhernandezb96@gmail.com, +53-5-426-8660)
+ * @author JesusHdezWaterloo@Github
  */
 public class TextToast extends ToastComponent implements ElevationEffect {
 
